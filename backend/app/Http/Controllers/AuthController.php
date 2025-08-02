6864439c3b2817a1bb03a277b5a9f6abbfb11988
Rajgohel0312 <rajgohel2018@gmail.com>
2025-08-02 10:11:27 +0530

    Added the Information of code

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -9,14 +9,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
+    // controller for registering the user
     public function register(Request $request)
     {
+        // Added user registration validation
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|unique:users',
             'password' => 'required|string|min:6',
             'role' => 'in:user,agent,admin'
         ]);
+        // create user using create method
 
         $user = User::create([
             'name' => $request->name,
@@ -24,26 +27,32 @@ class AuthController extends Controller
             'role' => $request->role ?? 'user',
             'password' => Hash::make($request->password),
         ]);
+        // return the registered user as json data
         return response()->json([
             'token' => $user->createToken('auth_token')->plainTextToken,
             'user' => $user
         ]);
     }
+    // Login Controller
     public function login(Request $request)
     {
+        // Validate email and password
         $request->validate([
             'email' => 'required|email',
             'password' => 'required|string|min:6'
         ]);
+        // Find entered user using email
         $user = User::where('email', $request->email)->first();
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages(['email' => ['The provided credentials are incorrect']]);
         }
+        // Return the user with the authentication token which is used for bearer token authentication
         return response()->json([
             'token' => $user->createToken('auth_token')->plainTextToken,
             'user' => $user
         ]);
     }
+    // Logout route to clear the user login token
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
@@ -51,6 +60,7 @@ class AuthController extends Controller
         return response()->json(['message' => 'Logged Out']);
     }
 
+    // Return the Logged in User profile
     public function me(Request $request)
     {
         return response()->json($request->user());
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,13 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/register', function () {
-    echo 'I am from register router';
-});
+
+
+// Post Route for Registering User
 Route::post('/register', [AuthController::class, 'register']);
+// Post Route for Login User
 Route::post('/login', [AuthController::class, 'login']);
 
+// Checked using sanctum user is logged in or not
 Route::middleware('auth:sanctum')->group(function () {
+    // Route for getting profile
     Route::get('/me', [AuthController::class, 'me']);
+    // Route for Logout user
     Route::post('/logout', [AuthController::class, 'logout']);
 });
